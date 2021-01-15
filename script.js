6f7095af7e0b341457d727c512bd271d0147af39
Steve Babb <steve.babb@outlook.com>
2021-01-14 19:03:11 -0600

    script is returning input text with save button

--- script.js
+++ script.js
@@ -12,45 +12,91 @@ $(document).ready(function () {
     $todaysDate.text(now);
 
     // Create empty string vars that correspond to all hours on the daily planner
-    var hour8 = "";
-    var hour9 = "";
-    var hour10 = "";
-    var hour11 = "";
-    var hour12 = "";
-    var hour13 = "";
-    var hour14 = "";
-    var hour15 = "";
-    var hour16 = "";
-    var hour17 = "";
+    var Save8 = $("#saveBtn8");
+    var Save9 = $("#saveBtn9");
+    var Save10 = $("#saveBtn10");
+    var Save11 = $("#saveBtn11");
+    var Save12 = $("#saveBtn12");
+    var Save13 = $("#saveBtn13");
+    var Save14 = $("#saveBtn14");
+    var Save15 = $("#saveBtn16");
+    var Save16 = $("#saveBtn16");
+    var Save17 = $("#saveBtn17");
 
-var hourInput = $("input");
+    var task8 = $("#input8")
+    var task9 = $("#input9");
+    var task10 = $("#input10");
+    var task11 = $("#input11");
+    var task12 = $("#input12");
+    var task13 = $("#input13");
+    var task14 = $("#input14");
+    var task15 = $("#input16");
+    var task16 = $("#input16");
+    var task17 = $("#input17");
+   
+    
+    var userTasks = [];
 
-// WHEN current date and time is set...
-// THEN the app needs to check what calendar hours are in the past, present, and future and color code accordingly.
-//  --get date and time (specifically, hour) from Moment()
-//  --check each time-block/hour for past, present, or future
-//  --set attributes for style/color
-var j=8;
-for (i=0; i < hourInput.length; i++) {
-    console.log(whichHour);
-    if (j == whichHour) {
-    $(hourInput[i]).addClass("present");
+    init();
+    function init() {
+        var storedSchedule = JSON.parse(localStorage.getItem("userSchedule"));
+        if (storedSchedule !== null) {
+            userTasks = storedSchedule;
+        }
+        console.log(userTasks);
     }
+
+    // WHEN current date and time is set...
+    // THEN the app needs to check what calendar hours are in the past, present, and future and color code accordingly.
+    //  --get date and time (specifically, hour) from Moment()
+    //  --check each time-block/hour for past, present, or future
+    //  --set attributes for style/color
+
+    var hourInput = $("input");
+
+    var j = 8;
+    for (i = 0; i < hourInput.length; i++) {
+        console.log(whichHour);
+        if (j == whichHour) {
+            $(hourInput[i]).addClass("present");
+        }
         else if (j < whichHour) {
             $(hourInput[i]).addClass("past");
         }
-            else {
-                $(hourInput[i]).addClass("future");
-            }
-    j++;
-}
+        else {
+            $(hourInput[i]).addClass("future");
+        }
+        j++;
+    }
 
     // WHEN time-blocks are colored...
     // THEN the app needs to check local storage and load the users saved items onto the calendar.
+
+
     //  --get local storage (keys and values).
     //  --append key values to their assigned description/time-blocks
 
     // WHEN user clicks the save button...
+
+    $("button").on("click", function(event) {
+        event.preventDefault();
+
+        var task8text = $(task8).val();
+        var task9text = $(task9).val();
+        var task10text = $(task10).val();
+        var task11text = $(task11).val();
+        var task12text = $(task12).val();
+        var task13text = $(task13).val();
+        var task14text = $(task14).val();
+        var task15text = $(task15).val();
+        var task16text = $(task16).val();
+        // var task17text = $(task17).val();
+
+        // console.log(task17text);
+        console.log(task16text);
+        
+        // localStorage.setItem("userSchedule", JSON.stringify(userTasks));
+    });
     // THEN the app needs to save user entry to local storage
     //  --Listen for click event on save button
     //  --set local storage (key and value) --> STORE BOTH TIME AND DESCRIPTION SO THAT NEXT APPEND KNOWS WHAT AND WHERE
